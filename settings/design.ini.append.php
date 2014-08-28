7295e81ebca1cab0b64f18b4d714c75545a55d29
Telemark fylkeskommune <telemark.fylkeskommune@gmail.com>
2014-08-28 08:31:31 +0200

    Changes for new version

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -1,6 +1,6 @@
 <?php /* #?ini charset="utf-8"?
 
 [ExtensionSettings]
-DesignExtensions[]=telemarksbarometeret
+DesignExtensions[]=www_tmb
 
 */ ?>
--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -1,6 +1,6 @@
 <?php /*
 
 [RegionalSettings]
-TranslationExtensions[]=telemarksbarometeret
+TranslationExtensions[]=tmb
 
 */ ?>
